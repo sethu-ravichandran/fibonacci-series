8d97be5e15affe6b678c1b4ccaf757fdf4578820
sethu-ravichandran <rsethu1997@gmail.com>
2023-03-23 15:09:20 +0530

    Add. Included the logic to print n number of Fibonacci digits.

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class FibonacciSeries {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+
+        int firstFibonacciNumber = 0, secondFibonacciNumber = 1, nextFibonacciNumber;
+
+        for (int iterator = 0; iterator < inputNumber; iterator++)
+        {
+            System.out.println(firstFibonacciNumber);
+            nextFibonacciNumber = firstFibonacciNumber + secondFibonacciNumber;
+            firstFibonacciNumber = secondFibonacciNumber;
+            secondFibonacciNumber = nextFibonacciNumber;
+        }
     }
 }
